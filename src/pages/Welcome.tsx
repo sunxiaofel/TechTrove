37ad494e7d46456cc97042224c360a957296c81a
sunxiaofei <sunxiaofei1422@gmail.com>
2024-07-10 13:42:24 +0800

    fix：路径问题

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -116,7 +116,7 @@ const Welcome: React.FC = () => {
               color: token.colorTextHeading,
             }}
           >
-            欢迎使用 Ant Design Pro
+            欢迎使用 Ant Design Pro 1
           </div>
           <p
             style={{
